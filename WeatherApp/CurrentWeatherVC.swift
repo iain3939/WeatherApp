b36794db6c2841efae8b72a9e60c14f97ab43d42
iain3939 <iain22@me.com>
2017-03-20 11:55:55 +0000

    More work on weather UI

--- WeatherApp/CurrentWeatherVC.swift
+++ WeatherApp/CurrentWeatherVC.swift
@@ -20,7 +20,31 @@ class CurrentWeatherVC: UIViewController, UICollectionViewDelegate, UICollection
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
            }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    
     
     
 
